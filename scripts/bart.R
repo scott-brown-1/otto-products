578ae2640ca1bad2799409357a6040c380fa5f42
Scott Brown <scottdbrown7@gmail.com>
2023-11-30 17:57:02 -0700

    Add BART and Naive Bayes

---
+++ scripts/bart.R
@@ -0,0 +1,111 @@
+#########################
+### Imports and setup ###
+#########################
+
+library(tidyverse)
+library(tidymodels)
+library(doParallel)
+
+#setwd('..')
+source('./scripts/utils.R')
+source('./scripts/feature_engineering.R')
+PARALLEL <- F
+FACTOR_CUTOFF <- 25
+
+#########################
+####### Load Data #######
+#########################
+
+## Load data
+data <- load_data(factor_cutoff=0)#FACTOR_CUTOFF)
+train <- data$train
+test <- data$test
+
+#########################
+## Feature Engineering ##
+#########################
+
+set.seed(2003)
+
+## parallel tune grid
+
+if(PARALLEL){
+  cl <- makePSOCKcluster(6)
+  registerDoParallel(cl)
+}
+
+## Set up preprocessing
+prepped_recipe <- setup_train_recipe(train, encode=F, poly=F, smote_K=0, pca_threshold=0)
+
+## Bake recipe
+bake(prepped_recipe, new_data=train)
+bake(prepped_recipe, new_data=test)
+
+#########################
+## Fit Classifier Model #
+#########################
+
+# Define model
+bart_model <- 
+  parsnip::bart(
+    trees = 10,
+    prior_terminal_node_coef = 0.75,#,tune(), #0.75,
+    prior_terminal_node_expo = 1.75,#tune()  #1.75,
+  ) %>% 
+  set_engine("dbarts") %>% 
+  set_mode("classification")
+
+## Define wf
+# Transform response to get different cutoff
+bart_wf <-
+  workflow(prepped_recipe) %>%
+  add_model(bart_model)
+
+# Grid of values to tune over
+tuning_grid <- grid_regular(
+  #trees(),
+  prior_terminal_node_coef(),
+  prior_terminal_node_expo(),
+  levels = 5
+)
+
+## Split data for CV
+folds <- vfold_cv(train, v = 5, repeats=1)
+
+## Run the CV
+cv_results <- bart_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(mn_log_loss))
+
+## Find optimal tuning params
+best_params <- cv_results %>%
+  select_best("mn_log_loss")
+
+print(best_params)
+
+## Fit workflow
+final_wf <- bart_wf %>%
+  #finalize_wf(best_params) %>%
+  fit(data = train)
+
+## Predict new y
+output <- predict(final_wf, new_data=test, type='prob') %>%
+  bind_cols(test$id,.) %>%
+  rename(
+    id=...1,
+    Class_1=.pred_Class_1,
+    Class_2=.pred_Class_2,
+    Class_3=.pred_Class_3,
+    Class_4=.pred_Class_4,
+    Class_5=.pred_Class_5,
+    Class_6=.pred_Class_6,
+    Class_7=.pred_Class_7,
+    Class_8=.pred_Class_8,
+    Class_9=.pred_Class_9)
+
+vroom::vroom_write(output,'./outputs/bart_preds.csv',delim=',')
+
+if(PARALLEL){
+  stopCluster(cl)
+}
---
+++ scripts/naive_bayes.R
@@ -0,0 +1,107 @@
+#########################
+### Imports and setup ###
+#########################
+
+library(tidyverse)
+library(tidymodels)
+library(doParallel)
+library(discrim)
+
+#setwd('..')
+source('./scripts/utils.R')
+source('./scripts/feature_engineering.R')
+PARALLEL <- T
+FACTOR_CUTOFF <- 25
+
+#########################
+####### Load Data #######
+#########################
+
+## Load data
+data <- load_data(factor_cutoff=0)#FACTOR_CUTOFF)
+train <- data$train
+test <- data$test
+
+#########################
+## Feature Engineering ##
+#########################
+
+set.seed(2003)
+
+## parallel tune grid
+
+if(PARALLEL){
+  cl <- makePSOCKcluster(6)
+  registerDoParallel(cl)
+}
+
+## Set up preprocessing
+prepped_recipe <- setup_train_recipe(train, encode=F, poly=F, smote_K=0, pca_threshold=0.85) #TODO: ENCODE! 
+
+## Bake recipe
+bake(prepped_recipe, new_data=train)
+bake(prepped_recipe, new_data=test)
+
+#########################
+## Fit Classifier Model #
+#########################
+
+## Define model
+bayes_model <- naive_Bayes(
+  Laplace=tune(),
+  smoothness=tune()) %>%
+  set_engine("naivebayes") %>%
+  set_mode("classification")
+
+## Define workflow
+bayes_wf <- workflow() %>%
+  add_recipe(prepped_recipe) %>%
+  add_model(bayes_model)
+
+## Grid of values to tune over
+tuning_grid <- grid_regular(
+  Laplace(),
+  smoothness(),
+  levels = 5)
+
+## Split data for CV
+folds <- vfold_cv(train, v = 5, repeats=1)
+
+## Run the CV
+cv_results <- bayes_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(mn_log_loss))
+
+## Find optimal tuning params
+best_params <- cv_results %>%
+  select_best("mn_log_loss")
+
+print(best_params)
+
+## Fit workflow
+final_wf <- bayes_wf %>%
+  finalize_workflow(best_params) %>%
+  fit(data = train)
+
+## Predict new y
+output <- predict(final_wf, new_data=test, type='prob') %>%
+  bind_cols(test$id,.) %>%
+  rename(
+    id=...1,
+    Class_1=.pred_Class_1,
+    Class_2=.pred_Class_2,
+    Class_3=.pred_Class_3,
+    Class_4=.pred_Class_4,
+    Class_5=.pred_Class_5,
+    Class_6=.pred_Class_6,
+    Class_7=.pred_Class_7,
+    Class_8=.pred_Class_8,
+    Class_9=.pred_Class_9)
+
+#LS: penalty, then mixture
+vroom::vroom_write(output,'./outputs/naive_bayes_preds.csv',delim=',')
+
+if(PARALLEL){
+  stopCluster(cl)
+}
